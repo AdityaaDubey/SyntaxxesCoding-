222926d314fdbbdca4681afed90fad6a27023315
AdityaaDubey <143491739+AdityaaDubey@users.noreply.github.com>
2023-10-13 15:13:21 +0530

    Create LenghtOfString.cpp
    the while loop continues iterating through the characters of the message string until it encounters the null character ('\0'). For each character encountered, the count variable is incremented, and the index is updated to move to the next character in the string. When the null character is encountered, the loop exits, and the value of count represents the length of the message string

---
+++ C++Program/BasicC++/LenghtOfString.cpp
@@ -0,0 +1,56 @@
+//Type 1:- WAP to calculate the length of a string WITHOUT using INBUILD funtion.
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Declaring and initializing a string variable
+    string greeting = "Hello, World!";
+    
+    // Concatenating strings
+    string name = "Alice";
+    string message = greeting + " My name is " + name;
+    cout << message << endl;
+
+    int count = 0;
+    int i = 0;  // Initialize i to 0
+
+    // Loop through the characters of the string until you encounter the null character
+    while (message[i] != '\0') {
+        count += 1;
+        i++; // Increment i to move to the next character
+    }
+
+    cout << "Length of message: " << count << endl;
+    return 0;
+}
+
+//Type 2:-  Using  INBUILD function.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Declaring and initializing a string variable
+    string greeting = "Hello, World!";
+    
+    // Concatenating strings
+    string name = "Alice";
+    string message = greeting + " My name is " + name;
+    
+    // Getting the length of the string
+    int length = message.length();
+    
+    // Printing the string and its length
+    cout << message << endl;
+    cout << "Length: " << length << endl;
+    
+    return 0;
+}
+
+
